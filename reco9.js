5cb8a8881cab3e02d47fd56bd40983696513bff5
Chris Pelling <cppelling@gmail.com>
2016-06-09 22:10:51 -0500

    added db connection params with error checking

--- reco9.js
+++ reco9.js
@@ -4,6 +4,12 @@
  * Recommendation engine built on top of neo4j
  */
 
-module.exports = function(param) {
-  return 'reco9' + param;
+module.exports = function(connectionUri, restUrl, restUsername, restPassword) {
+  if (arguments.length !== 4) throw 'reco9 expects 4 parameters: connectionUri, restUrl, restUsername, restPassword';
+  if (typeof connectionUri !== 'string') throw 'connectionUri is not a valid string';
+  if (typeof restUrl !== 'string') throw 'restUrl is not a valid string';
+  if (typeof restUsername !== 'string') throw 'restUsername is not a valid string';
+  if (typeof restPassword !== 'string') throw 'restPassword is not a valid string';
+
+  return connectionUri;
 };
--- test/test.js
+++ test/test.js
@@ -3,11 +3,52 @@
 var expect = require('chai').expect;
 var reco9 = require('../reco9');
 
-describe('#reco9', function() {
-  it('should add reco9 to the param', function() {
-    var result = reco9('test1');
-    expect(result).to.equal('reco9test1');
+describe('#reco9 tests', function() {
 
+
+  it('check that 4 params passed to reco9', function() {
+    try {
+      reco9();
+    }
+    catch(err) {
+      expect(err).to.equal('reco9 expects 4 parameters: connectionUri, restUrl, restUsername, restPassword');
+    }
+  });
+
+  it('check that connectionUri is a valid string', function() {
+    try {
+      reco9(7, "valid", "valid", "valid");
+    }
+    catch(err) {
+      expect(err).to.equal('connectionUri is not a valid string');
+    }
+  });
+
+  it('check that restUrl is a valid string', function() {
+    try {
+      reco9("valid", 7, "valid", "valid");
+    }
+    catch(err) {
+      expect(err).to.equal('restUrl is not a valid string');
+    }
+  });
+
+  it('check that restUsername is a valid string', function() {
+    try {
+      reco9("valid", "valid", 7, "valid");
+    }
+    catch(err) {
+      expect(err).to.equal('restUsername is not a valid string');
+    }
+  });
+
+  it('check that restPassword is a valid string', function() {
+    try {
+      reco9("valid", "valid", "valid", 7);
+    }
+    catch(err) {
+      expect(err).to.equal('restPassword is not a valid string');
+    }
   });
 
 });
