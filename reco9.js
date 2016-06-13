24963f35d785358f4aa22582252f7ab0af079248
Chris Pelling <cppelling@gmail.com>
2016-06-12 21:22:26 -0500

    updated config to read from file

--- reco9.js
+++ reco9.js
@@ -4,12 +4,23 @@
  * Recommendation engine built on top of neo4j
  */
 
-module.exports = function(connectionUri, restUrl, restUsername, restPassword) {
-  if (arguments.length !== 4) throw 'reco9 expects 4 parameters: connectionUri, restUrl, restUsername, restPassword';
-  if (typeof connectionUri !== 'string') throw 'connectionUri is not a valid string';
-  if (typeof restUrl !== 'string') throw 'restUrl is not a valid string';
-  if (typeof restUsername !== 'string') throw 'restUsername is not a valid string';
-  if (typeof restPassword !== 'string') throw 'restPassword is not a valid string';
+var reco9 = {};
 
-  return connectionUri;
-};
+reco9.loadConfigFile = function(path) {
+  var config = require(path);
+  var reco9config = config.reco9;
+  this.loadConfigJson(reco9config);
+}
+
+reco9.loadConfigJson = function(reco9config) {
+  this.neo4j = reco9config.neo4j;
+
+  if (typeof this.neo4j.connectionUri !== 'string') throw 'connectionUri is not a valid string';
+  if (typeof this.neo4j.restUrl !== 'string') throw 'restUrl is not a valid string';
+  if (typeof this.neo4j.restUsername !== 'string') throw 'restUsername is not a valid string';
+  if (typeof this.neo4j.restPassword !== 'string') throw 'restPassword is not a valid string';
+}
+
+
+
+module.exports = reco9;
--- test/test.js
+++ test/test.js
@@ -6,49 +6,11 @@ var reco9 = require('../reco9');
 describe('#reco9 tests', function() {
 
 
-  it('check that 4 params passed to reco9', function() {
-    try {
-      reco9();
-    }
-    catch(err) {
-      expect(err).to.equal('reco9 expects 4 parameters: connectionUri, restUrl, restUsername, restPassword');
-    }
+  it('check that loading config file works', function() {
+      reco9.loadConfigFile("./config.json");
+      expect(reco9.neo4j.connectionUri).to.equal('asdf');
   });
 
-  it('check that connectionUri is a valid string', function() {
-    try {
-      reco9(7, "valid", "valid", "valid");
-    }
-    catch(err) {
-      expect(err).to.equal('connectionUri is not a valid string');
-    }
-  });
 
-  it('check that restUrl is a valid string', function() {
-    try {
-      reco9("valid", 7, "valid", "valid");
-    }
-    catch(err) {
-      expect(err).to.equal('restUrl is not a valid string');
-    }
-  });
-
-  it('check that restUsername is a valid string', function() {
-    try {
-      reco9("valid", "valid", 7, "valid");
-    }
-    catch(err) {
-      expect(err).to.equal('restUsername is not a valid string');
-    }
-  });
-
-  it('check that restPassword is a valid string', function() {
-    try {
-      reco9("valid", "valid", "valid", 7);
-    }
-    catch(err) {
-      expect(err).to.equal('restPassword is not a valid string');
-    }
-  });
 
 });
